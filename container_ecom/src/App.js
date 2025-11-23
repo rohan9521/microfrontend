7ecd4989c7d5b0c9b3942822e82cbe5ac626a62d
Raja Rohan Ray <rajaro@Rajas-Mac-mini.local>
2025-11-24 00:11:52 +0530

    Updated app.js

--- container_ecom/src/App.js
+++ container_ecom/src/App.js
@@ -3,7 +3,7 @@ import MarketngApp from './components/MarketingApp';
 export const App = () => {
   return (
     <>
-      <div>Container with region updated</div>
+      <div>Container with region</div>
       <MarketngApp />
     </>
   );
