0ba003c5df24c294098b61df67b805e7c4034087
Raja Rohan Ray <rajaro@Rajas-Mac-mini.local>
2025-11-24 01:15:40 +0530

    Updated app.js 5

--- container_ecom/src/App.js
+++ container_ecom/src/App.js
@@ -3,7 +3,7 @@ import MarketngApp from './components/MarketingApp';
 export const App = () => {
   return (
     <>
-      <div>Container2</div>
+      <div>Container3</div>
       <MarketngApp />
     </>
   );
