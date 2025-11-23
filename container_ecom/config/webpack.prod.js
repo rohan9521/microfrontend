394042d4b4498752bdcdc0f329f5a21d18ba2f5f
Raja Rohan Ray <rajaro@Rajas-Mac-mini.local>
2025-11-24 00:40:17 +0530

    Updated app.js 4

--- container_ecom/config/webpack.prod.js
+++ container_ecom/config/webpack.prod.js
@@ -8,7 +8,7 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: "production",
   output: {
-    fileName: "[name].[contenthash].js",
+    filename: "[name].[contenthash].js",
     publicPath: "/container/latest",
   },
   plugins: [
