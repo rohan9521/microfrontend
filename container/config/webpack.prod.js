40848019632611aa5cbdfa911b278f6102842c0f
Raja Rohan Ray <rajaro@Rajas-Mac-mini.local>
2025-12-02 00:06:46 +0530

    Marketing remoteentry.js url update

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -2,8 +2,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
-
-const domain = process.env.PRODUCTION_DOMAIN;
+const webpack = require("webpack");
 
 const prodConfig = {
   mode: "production",
@@ -11,10 +10,13 @@ const prodConfig = {
     filename: "[name].[contenthash].js",
   },
   plugins: [
+    new webpack.DefinePlugin({
+    "process.env.PRODUCTION_DOMAIN": JSON.stringify(process.env.PRODUCTION_DOMAIN),
+  }),
     new ModuleFederationPlugin({
       name: "container_ecom",
       remotes: {
-        "marketing": `marketing@${domain}/marketing/remoteEntry.js`,
+        "marketing": `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     })
